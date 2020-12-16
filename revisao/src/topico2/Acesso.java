23d386e13fc360db7450661000eb927540dd33f1
Bruna Rossini <brunarossini94@gmail.com>
2020-12-16 16:12:20 -0300

    Inserida a classe Acesso com seu construtur e métodos de acesso e modificaçao

--- revisao/src/topico2/Acesso.java
+++ revisao/src/topico2/Acesso.java
@@ -5,7 +5,28 @@ import java.time.LocalDateTime;
 public class Acesso {
 	
 	private LocalDateTime login;
-	private LocalDateTime senha;
+	private LocalDateTime logout;
+	
+	public Acesso(LocalDateTime login, LocalDateTime logout) {
+		this.login = login;
+	}
+
+	public LocalDateTime getLogin() {
+		return login;
+	}
+
+	public void setLogin(LocalDateTime login) {
+		this.login = login;
+	}
+
+	public LocalDateTime getLogout() {
+		return logout;
+	}
+
+	public void setLogout(LocalDateTime logout) {
+		this.logout = logout;
+	}
+	
 	
 
 }
