5a17a3a93e85d8380d42e2af4038c79d435adc46
BrunaRossini <brunarossini94@gmail.com>
2020-12-18 17:14:36 -0300

    Criada Classe Pessoa

---
+++ revisao/src/sistemabiblioteca/Pessoa.java
@@ -0,0 +1,13 @@
+package sistemabiblioteca;
+
+public abstract class Pessoa {
+	
+	private long cpf;
+    private String email;
+    private String login;
+    private date nascimento;
+	private String nome;
+	private String senha;
+	private String sobrenome;
+
+}
